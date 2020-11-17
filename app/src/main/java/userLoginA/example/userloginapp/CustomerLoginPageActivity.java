11505ff704d9299e92ecbffd8a3dd10fba078b4b
Peleg Z <peleg122@gmail.com>
2020-11-17 12:55:09 +0200

    Testing Commit 2

--- app/src/main/java/userLoginA/example/userloginapp/CustomerLoginPageActivity.java
+++ app/src/main/java/userLoginA/example/userloginapp/CustomerLoginPageActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class CustomerLoginPageActivity extends AppCompatActivity {
-
+    //test
     private Button customerRegisterBtn;
     private Button customerLoginBtn;
 
